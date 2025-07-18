931e74db67d7eb2fb98c9d8446d1ab6581312660
Om Deore <oninvis@MacBook-Pro.local>
2025-07-19 00:20:31 +0530

    Added the batching and tokenize function and load datasets function

---
+++ steering_vecto_fun.py
@@ -0,0 +1,24 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+def load_datasets(dataset_path):
+    with open(dataset_path) as f:
+        text = [l.strip() for l in f if l.strip()]
+        return text 
+
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+model = AutoModelForCausalLM.from_pretrained('bert-base-uncased',output_hidden_states=True)
+def tokenize_and_batch(text , batch_size:16):
+    device = torch.device('cude' if torch.cuda.is_available() else 'cpu')
+    for i in range(0 , len(text) , batch_size):
+        batch_texts = text[i:i+batch_size]
+        batch_tokens = tokenizer(batch_texts , padding=True , truncation=True , return_tensors='pt').to(device)
+        return batch_tokens
+    
+    model.eval()
+    model.to(device)
+
+
+
+    
+
