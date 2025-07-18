4b08390b49c230e401cceb1a733cea7db6f6e2d4
Om Deore <oninvis@MacBook-Pro.local>
2025-07-19 00:29:04 +0530

    just tweaked a lil

--- steering_vecto_fun.py
+++ steering_vecto_fun.py
@@ -7,16 +7,17 @@ def load_datasets(dataset_path):
         return text 
 
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+device = torch.device('cude' if torch.cuda.is_available() else 'cpu')
 model = AutoModelForCausalLM.from_pretrained('bert-base-uncased',output_hidden_states=True)
-def tokenize_and_batch(text , batch_size:16):
-    device = torch.device('cude' if torch.cuda.is_available() else 'cpu')
+model.eval()
+model.to(device)
+def tokenize_and_batch(text , batch_size: int = 16):
     for i in range(0 , len(text) , batch_size):
         batch_texts = text[i:i+batch_size]
         batch_tokens = tokenizer(batch_texts , padding=True , truncation=True , return_tensors='pt').to(device)
         return batch_tokens
     
-    model.eval()
-    model.to(device)
+    
 
 
 
