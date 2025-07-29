e339a2a5441f7532b3e22a9e1cd6ad5bba62487b
Farhan Kittur <kitturfarhan@gmail.com>
2025-07-28 22:18:26 -0400

    Changes made in accordance to Lovkush's Comments

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-import json
-
-#Loads a dataset in the format of the BBQ files and converts it to a list of strings
-def load_BBQ_dataset(dataset_path):
-    data = []
-
-    with open(dataset_path, 'r') as f:
-        for line in f:
-            # Each line is a JSON object
-            json_obj = json.loads(line.strip())
-            data.append(json_obj)
-
-    # Now 'data' is a list of Python dictionaries, each representing a JSON object from the file
-    def get_question(dataSpot):
-        output = ""
-        output += dataSpot['context'] + " "
-        output += dataSpot['question'] + " "
-        output += 'Pick one of three options: '
-        output += dataSpot['ans0'] + ", "
-        output += dataSpot['ans1'] + ", "
-        output += dataSpot['ans2']
-        return output
-    return [get_question(x) for x in data]
 
 # Convert datasets into a list of strings
 def load_datasets(dataset_path):
