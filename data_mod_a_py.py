2fe07b1a9046cd477b93f4cdae85ce7c7c8a5165
mistovek016 <aryaman.aisola@gmail.com>
2025-08-14 20:43:48 +0000

    more exp

--- data_mod_a_py.py
+++ data_mod_a_py.py
@@ -5,10 +5,11 @@ from os import path
 root = get_repo_root()
 data_path = path.join(root, "BBQ_Prompt_Sets", "Nationality.jsonl")
 data = load_bbq_dataset(data_path)
-neutral = []
-opinion = []
+# neutral = []
+# opinion = []
+bbq = []
 for i in range(10):
-    neutral.append(data[i*10])
-    opinion.append(data[i*10 + 1])
-
-print(opinion)
+    # neutral.append(data[i*10])
+    # opinion.append(data[i*10 + 1])
+    bbq.append(data[i*10])
+    bbq.append(data[i*10 + 1])
